0793dafc68846d395972d6156fb5ce0c9d8f4be3
Shashi Essp <shashiessp@gmail.com>
2014-04-26 05:03:26 +0530

    added index.html

--- iboilerplate/urls.py
+++ iboilerplate/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,5 +12,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    (r'^', TemplateView.as_view(template_name="index.html")),
 
 )
